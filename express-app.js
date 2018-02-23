90cf51aad3b53ed698363d9538db4c9d3c466df4
sharath <sharath.r@ge.com>
2018-02-23 19:54:34 +0530

    express v2

--- express-app.js
+++ express-app.js
@@ -1,6 +1,31 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello World!'));
+/*app.get('/', (req, res) => res.send('Hello World!'));
+ */
+//res.write('Hello World!')
+//res.end()
+app.get('/', function (req, res) {
+    res.send('Hello World!')
+});
+app.get('/list', function (req, res) {
+    var blocks = ['t', '2', 'rr'];
+    res.send(blocks);
+});
+app.get('/jlist', function (req, res) {
+    var blocks = ['t', '2', 'rr'];
+    res.json(blocks);
+});
+app.get('/html', function (req, res) {
+    var blocks = "<h1>Sharath R</h1>";
+    res.send(blocks);
+});
+
+app.get('/app', function (req, res) {
+    res.redirect('/');
+    //moved temp : 302 header 
+    //res.redirect(301, '/');
+});
+
 
 app.listen(8888, () => console.log('Example app listening on port 8888!'));
