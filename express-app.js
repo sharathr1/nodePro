f5eeff25bb101a83b190e2add212c75f7ba80036
sharath <sharath.r@ge.com>
2018-02-26 21:36:58 +0530

    mango db

--- express-app.js
+++ express-app.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const app = express();
 
+var MongoClient = require('mongodb').MongoClient;
+
+//CSS folder 
+/*app.get('/css/*', function (req, res) {
+    res.sendFile(__dirname + "/css/style.css")
+});*/
+app.use(express.static("public"));
+
+
+
 /*app.get('/', (req, res) => res.send('Hello World!'));
  */
 //res.write('Hello World!')
@@ -26,6 +36,34 @@ app.get('/app', function (req, res) {
     //moved temp : 302 header 
     //res.redirect(301, '/');
 });
+app.get('/index', function (req, res) {
+    res.sendFile(__dirname + "/index.html")
+    //moved temp : 302 header 
+    //res.redirect(301, '/');
+});
 
 
+
+
+var url = "mongodb://localhost:27017/";
+
+app.get('/db', function (req, res) {
+    var dbres;
+    console.log("Calling DB");
+
+    MongoClient.connect(url, function (err, db) {
+        if (err) throw err;
+        var dbo = db.db("testmangodb");
+        dbo.collection("teams").findOne({}, function (err, result) {
+            if (err) throw err;
+            console.log(result);
+            dbres = result;
+
+            res.json(dbres);
+
+        });
+        db.close();
+    });
+    console.log("completed DB query");
+});
 app.listen(8888, () => console.log('Example app listening on port 8888!'));
